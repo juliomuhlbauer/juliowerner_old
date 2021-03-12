7d74db63cdefab23ec83f7c668bc44ab5b874489
Julio Werner <juliowernermm@gmail.com>
2021-03-12 14:23:01 -0300

    📈 Google Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,8 +2,21 @@ import { ChakraProvider } from "@chakra-ui/react";
 import customTheme from "@/styles/theme";
 import "swiper/swiper-bundle.css";
 import { DefaultSeo } from "next-seo";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "@/lib/gtag";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
       <DefaultSeo
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -2,6 +2,7 @@ import { ColorModeScript } from "@chakra-ui/react";
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import customTheme from "@/styles/theme";
 import { NextSeo } from "next-seo";
+import { GA_TRACKING_ID } from "@/lib/gtag";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -17,7 +18,25 @@ class MyDocument extends Document {
           href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700;900&display=swap"
           rel="stylesheet"
         />
-        <Head />
+        <Head>
+          {/* Global Site Tag (gtag.js) - Google Analytics */}
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
+        </Head>
         <body>
           <ColorModeScript
             initialColorMode={customTheme.config.initialColorMode}
