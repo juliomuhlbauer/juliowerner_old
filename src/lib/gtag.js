1875b28b21eeead5f2eaf51bf20fbcaa1a689d75
Julio Werner <juliowernermm@gmail.com>
2021-03-12 16:01:10 -0300

    ⚙ Google Tag Manager

--- src/lib/gtag.js
+++ src/lib/gtag.js
@@ -1,4 +1,4 @@
-export const GA_TRACKING_ID = "G-FMTZHNNRCR";
+export const GA_TRACKING_ID = "G-Y65LQMWMC6";
 
 // https://developers.google.com/analytics/devguides/collection/gtagjs/pages
 export const pageview = (url) => {
