3b55c1edfdb05de26851d47259d925d9bdbf9a89
Julio Werner <juliowernermm@gmail.com>
2021-03-31 23:49:24 -0300

    🔥 Projects to Airtable

--- src/pages/br/projetos.js
+++ src/pages/br/projetos.js
@@ -1,9 +1,37 @@
 import { Center, Link, SimpleGrid } from "@chakra-ui/react";
 import Card from "@/components/card";
 import Page from "@/components/page";
-import { ProjectsData } from "@/data/projects";
+import Airtable from "airtable";
 
-export default function Projetos() {
+export async function getStaticProps() {
+  const airtable = new Airtable({
+    apiKey: process.env.AIRTABLE_API_KEY,
+  });
+
+  const records = await airtable
+    .base("appG7EIhRu9l6fKVI")("Projects")
+    .select({
+      fields: ["Name", "Image", "Link"],
+    })
+    .all();
+
+  const projects = records.map((project) => {
+    return {
+      name: project.get("Name"),
+      image: project.get("Image"),
+      link: project.get("Link"),
+    };
+  });
+
+  return {
+    props: {
+      projects,
+    },
+    revalidate: 3600,
+  };
+}
+
+export default function Projetos({ projects }) {
   return (
     <Page title="Projetos">
       <Center as="section" h={{ base: "100%", xl: "75vh" }}>
@@ -13,13 +41,13 @@ export default function Projetos() {
           justify="center"
           spacing={12}
         >
-          {ProjectsData.map((ProjectsData, index) => (
+          {projects.map((projects, index) => (
             <Card
               key={index}
-              link={ProjectsData.link}
-              title={ProjectsData.title}
-              image={ProjectsData.image}
-              alt={ProjectsData.title}
+              link={projects.link}
+              title={projects.name}
+              image={projects.image}
+              alt={projects.name}
             />
           ))}
         </SimpleGrid>
