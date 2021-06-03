55c66e73977c149f6211d3e52db5b970bd3687f7
Julio Werner <juliowernermm@gmail.com>
2021-06-03 10:42:18 -0300

    🔗 Redirect

---
+++ src/pages/teste.js
@@ -0,0 +1,10 @@
+import { Text } from "@chakra-ui/layout";
+import { Skeleton } from "@chakra-ui/skeleton";
+
+export default function Teste() {
+  return (
+    <Skeleton isLoaded>
+      <Text>toajdlkajdo~saj daskdjaskldj asjdklajsdlkjaksdçjsoçadjsadj</Text>
+    </Skeleton>
+  );
+}
