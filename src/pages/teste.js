f8f7dfa09421a81dc9fcaafee7b90636cef4aa83
Julio Werner <juliowernermm@gmail.com>
2021-06-03 10:54:49 -0300

    👀 Testing

--- src/pages/teste.js
+++ src/pages/teste.js
@@ -1,9 +1,11 @@
 import { Text } from "@chakra-ui/layout";
 import { Skeleton } from "@chakra-ui/skeleton";
 
+let test;
+
 export default function Teste() {
   return (
-    <Skeleton isLoaded>
+    <Skeleton isLoaded={test}>
       <Text>toajdlkajdo~saj daskdjaskldj asjdklajsdlkjaksdçjsoçadjsadj</Text>
     </Skeleton>
   );
