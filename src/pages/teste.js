e4d223f9851d86ce70d7f57c62bff6643c8de93e
Julio Werner <juliowernermm@gmail.com>
2021-06-03 11:04:43 -0300

    test

--- src/pages/teste.js
+++ src/pages/teste.js
@@ -6,35 +6,35 @@ let projects = {
   loaded: false,
 };
 
-// export async function getServerSideProps() {
-//   const airtable = new Airtable({
-//     apiKey: process.env.AIRTABLE_API_KEY,
-//   });
+export async function getServerSideProps() {
+  const airtable = new Airtable({
+    apiKey: process.env.AIRTABLE_API_KEY,
+  });
 
-//   const records = await airtable
-//     .base("appG7EIhRu9l6fKVI")("Projects")
-//     .select()
-//     .all();
+  const records = await airtable
+    .base("appG7EIhRu9l6fKVI")("Projects")
+    .select()
+    .all();
 
-//   let projects = records.map((project) => {
-//     return {
-//       name: project.get("Name") || null,
-//       image: project.get("Image") || null,
-//       link: project.get("Link") || null,
-//       status: project.get("Status") || null,
-//       order: project.get("Order") || null,
-//       color: project.get("Color") || null,
-//       bg: project.get("BG") || null,
-//       loaded: true,
-//     };
-//   });
+  let projects = records.map((project) => {
+    return {
+      name: project.get("Name") || null,
+      image: project.get("Image") || null,
+      link: project.get("Link") || null,
+      status: project.get("Status") || null,
+      order: project.get("Order") || null,
+      color: project.get("Color") || null,
+      bg: project.get("BG") || null,
+      loaded: true,
+    };
+  });
 
-//   return {
-//     props: {
-//       projects,
-//     },
-//   };
-// }
+  return {
+    props: {
+      projects,
+    },
+  };
+}
 
 export default function Teste() {
   return (
