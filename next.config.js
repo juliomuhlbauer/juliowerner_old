d2314b49fb97d4433d65030f309989e8f11f58d4
Julio Werner <juliowernermm@gmail.com>
2021-06-19 11:28:34 -0300

    🔗 Gtag

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 module.exports = {
   target: "serverless",
   reactStrictMode: true,
-  env: {
-    GA_TRACKING_ID: process.env.GA_TRACKING_ID,
-  },
 };
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -45,14 +45,14 @@ function MyApp({ Component, pageProps }) {
       </Head>
 
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_TRACKING_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
       />
 
       <Script>
         {`window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${process.env.NEXT_PUBLIC_GA_TRACKING_ID}', {
+          gtag('config', '${GA_TRACKING_ID}', {
             page_path: window.location.pathname,
           });`}
       </Script>
