510129ae43199f848567836f2a83d8def7bc8ce0
Julio Werner <juliowernermm@gmail.com>
2021-06-18 22:55:56 -0300

    👌 Renaming

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,7 +10,7 @@ import {
   Link,
   Stack,
 } from "@chakra-ui/react";
-import Nav from "@/components/Nav";
+import Nav from "@/components/Navigation";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 
 const AboutData = [
