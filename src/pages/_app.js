4b09b52a2e0d8b8477a46f8bd37b9c4b2c2b8e01
Julio Werner <juliowernermm@gmail.com>
2021-06-18 23:12:20 -0300

    🔗 Env

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -45,14 +45,14 @@ function MyApp({ Component, pageProps }) {
       </Head>
 
       <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.GA_TRACKING_ID}`}
       />
 
       <Script>
         {`window.dataLayer = window.dataLayer || [];
           function gtag(){dataLayer.push(arguments);}
           gtag('js', new Date());
-          gtag('config', '${GA_TRACKING_ID}', {
+          gtag('config', '${process.env.GA_TRACKING_ID}', {
             page_path: window.location.pathname,
           });`}
       </Script>
